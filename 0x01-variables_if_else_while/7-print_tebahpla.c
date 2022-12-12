c3635ee55a9083c839d1c270bf21436760d5985f
Joshua Kalule <kaljoshkalule@gmail.com>
2022-12-12 23:36:12 +0000

    add file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
