ae6a0e0373ee931b360b5eb7bc1bdf8ee611985a
kiizq <gaylzlara2@gmail.com>
2022-12-12 05:34:23 -0800

    comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,22 +13,20 @@ int main(void)
 	int ten;
 	int bas;
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
-
-	for (hun = 0 ; hun <= 10 ; hun++)
+for (tho = 0 ; tho < 100 ; tho++)
+{
+	for (hun = 0 ; hun <= 100 ; hun++)
 	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
+		for (ten = 0 ; ten < 100 ; ten++)
 		{
-			for ( bas = 0 ; bas < 10 ; bas++)
+			for ( bas = 0 ; bas < 100 ; bas++)
 			{
 				putchar('0' + tho);
 				putchar('0' + hun);
 				putchar(32);
 				putchar('0' + ten);
 				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
+				if (!(tho == 99 && hun == 88))
 				{
 					putchar(',');
 					putchar(32);
