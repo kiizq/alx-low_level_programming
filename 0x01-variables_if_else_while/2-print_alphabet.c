19a6f9da7bde487aca00a245f43e09c58ce1f473
kiizq <gaylzlara2@gmail.com>
2022-12-12 03:06:32 -0800

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
 int main(void)
 
 {
-	char lc;
+	char c;
 
-	for (lc = 'a'; lc <= 'z'; lc++);
+	c = 'a';
+	while (c <= 'z');
 
-	{	putchar(lc);
+	{	putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);
