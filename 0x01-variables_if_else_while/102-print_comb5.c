9ab058ac8521d2e5072806d1135dc649a87bf6a0
kiizq <gaylzlara2@gmail.com>
2022-12-12 04:04:27 -0800

    print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Succes)
+ * main - Entry
+ * Return: Always 0
  */
 
 int main(void)
-{
-	int tho;
-	int hun;
-	int ten;
-	int bas;
-
-for (tho = 0 ; tho < 10 ; tho++)
-{
-
-	for (hun = 0 ; hun <= 10 ; hun++)
+{	int a;
+	for (a = 0; a < 10; a++)
 	{
-		for (ten = 0 ; ten < 10 ; ten++)
+		putchar(a + '0');
+		if (a < 9)
 		{
-			for (bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }
