c92766540a3f47c53aeb167b4ca8d10f86b9d3a0
Joshua Kalule <kaljoshkalule@gmail.com>
2022-12-13 23:57:11 +0000

    add file 1-alphabet.c and update main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: int
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(c);
+}
+
+
