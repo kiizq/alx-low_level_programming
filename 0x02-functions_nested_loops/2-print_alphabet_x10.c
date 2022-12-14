d370a455eabcedcf747164acd354138716adc994
Joshua Kalule <kaljoshkalule@gmail.com>
2022-12-14 00:14:12 +0000

    add file 2 and updated main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase
+ *
+ * Return: int
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
+
