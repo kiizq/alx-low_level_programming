375cb884ecd50bc14b92f482c7d3618fbb3bda46
Joshua Kalule <kaljoshkalule@gmail.com>
2022-12-14 21:54:14 +0000

    add file 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int count = 1;
+	unsigned long int n1 = 1;
+	unsigned long int n2 = 2;
+	unsigned long int tmp;
+
+	printf("%d, %d, ", n1, n2);
+	count = 3;
+	while (count < 50)
+	{
+		tmp = n1 + n2;
+		printf("%lu", tmp);
+		n1 = n2;
+		n2 = tmp;
+		count++;
+		if (count != 50)
+			printf(", ");
+	}
+	return (0);
+}
