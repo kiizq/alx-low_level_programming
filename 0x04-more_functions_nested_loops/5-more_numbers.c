24eb1d633b707a396e39e942932ba07eb9b05304
Joshua Kalule <kaljoshkalule@gmail.com>
2022-12-15 13:16:15 +0000

    add file 5 and update main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+void print_0_14(void);
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14,
+ *                 followed by a new line
+ */
+void more_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		print_0_14();
+		_putchar('\n');
+	}
+}
+
+/**
+ * print_0_14 - prints 0 to 14
+ */
+void print_0_14(void)
+{
+	int n;
+	char c1;
+	char c2;
+
+	for (n = 0; n <= 14; n++)
+	{
+		c1 = n / 10;
+		c2 = n % 10;
+
+		if (c1 == 1)
+			_putchar(c1 + '0');
+		_putchar(c2 + '0');
+
+	}
+}
+
