3f36458ffb9d27ba82489a3a6e2866e29ffa6513
kiizq <gaylzlara2@gmail.com>
2022-12-18 12:05:55 -0800

    advanced tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: number to print
+ */
+
+void print_number(int n)
+{
+	double divider, next;
+	int ans, i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	divider = 10.0;
+	next = n;
+
+	do {
+		divider *= 10;
+
+	} while ((int)(next / divider) > 0);
+
+	i = 10;
+	while (i <= divider)
+	{
+		ans = (int)((n / divider) * i) % 10;
+		if (i == 10)
+		{
+			if (ans != 0)
+				_putchar(ans + '0');
+		}
+		else
+		{
+			_putchar(ans + '0');
+		}
+		i *= 10;
+	}
+}
