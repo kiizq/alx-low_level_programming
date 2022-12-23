ff174fb6791546cc5b6f9f62baa58da4c68eba4e
Joshua Kalule <kaljoshkalule@gmail.com>
2022-12-23 00:49:28 +0000

    add file 100 and update main.h

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @c: string
+ *
+ * Return: pointer to c
+ */
+char *rot13(char *c)
+{
+	int i = 0, j;
+	char expected[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+			'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+			'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			'y', 'z'};
+	char wanted[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
+		'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	while (c[i] != '\0')
+	{
+		j = 0;
+		while (expected[j] != '\0')
+		{
+			if (c[i] == expected[j])
+			{
+				c[i] = wanted[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,3 +19,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 
 char *leet(char *);
+
+char *rot13(char *);
