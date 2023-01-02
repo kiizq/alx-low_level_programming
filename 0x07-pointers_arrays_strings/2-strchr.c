911ddd9b8a04f139f4feef6a0a6f55dd11d4ef53
Joshua Kalule <kaljoshkalule@gmail.com>
2023-01-02 10:05:28 +0000

    add file 2-strchr.c and update main.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character being looked for
+ *
+ * Return: pointer to the first occurance of c
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
